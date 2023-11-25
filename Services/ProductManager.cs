71a8b2f6cdb86f74e5d2de487474a4e8ac8527b1
Osman Erdem Kılıç <osman_erdem1200@hotmail.com>
2023-11-25 14:25:45 +0300

    Delete

--- Services/ProductManager.cs
+++ Services/ProductManager.cs
@@ -15,8 +15,19 @@ namespace Services
 
         public void CreateProduct(Product product)
         {
-            _manager.Product.CreateProduct(product);
+            _manager.Product.CreateOneProduct(product);
             _manager.Save();
+        }
+
+        public void DeleteOneProduct(int id)
+        {
+            Product product = GetOneProduct(id, false);
+            if (product is not null)
+            {
+                _manager.Product.DeleteOneProduct(product);
+                _manager.Save();
+            }
+            
         }
 
         public IEnumerable<Product> GetAllProducts(bool trackChanges)
--- StoreApp/Areas/Admin/Controllers/ProductController.cs
+++ StoreApp/Areas/Admin/Controllers/ProductController.cs
@@ -55,5 +55,12 @@ namespace StoreApp.Areas.Admin.Controllers
             return View();
          
         }
+
+
+        public IActionResult Delete([FromRoute(Name ="id")]int id)
+        {
+            _manager.ProductService.DeleteOneProduct(id);
+            return RedirectToAction("Index");
+        }
     }
 }
