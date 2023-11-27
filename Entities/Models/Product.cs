1a19c2f068ec604c9385f54f645b0a5c54213e6e
Osman Erdem Kılıç <osman_erdem1200@hotmail.com>
2023-11-27 12:24:29 +0300

    Product and Category

--- Entities/Models/Product.cs
+++ Entities/Models/Product.cs
@@ -10,4 +10,6 @@ public class Product
 
     [Required(ErrorMessage ="Price is not empty")]
     public decimal Price { get; set; }
+    public int? CategoryId { get; set; }
+    public Category? Category { get; set; }
 }
